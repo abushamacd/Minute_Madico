a10d0b8d9c2c64e431e608db6b4370404189ee72
Shama <assiddik001@gmail.com>
2022-12-14 18:50:26 +0600

    dashboard

--- components/dashboard/Cards/CardLineChart.js
+++ components/dashboard/Cards/CardLineChart.js
@@ -26,8 +26,8 @@ export default function CardLineChart() {
           {
             label: new Date().getFullYear() - 1,
             fill: false,
-            backgroundColor: "#fff",
-            borderColor: "#fff",
+            backgroundColor: "#333",
+            borderColor: "#333",
             data: [40, 68, 86, 74, 56, 60, 87],
           },
         ],
@@ -59,7 +59,7 @@ export default function CardLineChart() {
           xAxes: [
             {
               ticks: {
-                fontColor: "rgba(255,255,255,.7)",
+                fontColor: "#333",
               },
               display: true,
               scaleLabel: {
@@ -71,7 +71,7 @@ export default function CardLineChart() {
                 display: false,
                 borderDash: [2],
                 borderDashOffset: [2],
-                color: "rgba(33, 37, 41, 0.3)",
+                color: "#ddd",
                 zeroLineColor: "rgba(0, 0, 0, 0)",
                 zeroLineBorderDash: [2],
                 zeroLineBorderDashOffset: [2],
@@ -81,7 +81,7 @@ export default function CardLineChart() {
           yAxes: [
             {
               ticks: {
-                fontColor: "rgba(255,255,255,.7)",
+                fontColor: "#333",
               },
               display: true,
               scaleLabel: {
@@ -93,8 +93,8 @@ export default function CardLineChart() {
                 borderDash: [3],
                 borderDashOffset: [3],
                 drawBorder: false,
-                color: "rgba(255, 255, 255, 0.15)",
-                zeroLineColor: "rgba(33, 37, 41, 0)",
+                color: "#ddd",
+                zeroLineColor: "#ddd",
                 zeroLineBorderDash: [2],
                 zeroLineBorderDashOffset: [2],
               },
@@ -108,14 +108,14 @@ export default function CardLineChart() {
   }, []);
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
+      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white">
         <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
           <div className="flex flex-wrap items-center">
             <div className="relative w-full max-w-full flex-grow flex-1">
-              <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
+              <h6 className="uppercase text-white mb-1 text-xs font-semibold">
                 Overview
               </h6>
-              <h2 className="text-white text-xl font-semibold">Sales value</h2>
+              <h2 className="text-black text-xl font-semibold">Sales value</h2>
             </div>
           </div>
         </div>
