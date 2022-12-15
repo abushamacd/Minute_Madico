8ac35bedc108b55a273c56e11fdb8b8d0169f9c0
Shama <assiddik001@gmail.com>
2022-12-15 18:42:20 +0600

    sidebar fix

--- sections/dashboard/Sidebar.js
+++ sections/dashboard/Sidebar.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import NotificationDropdown from "../../components/dashboard/NotificationDropdown";
 import UserDropdown from "../../components/dashboard/UserDropdown";
-import { FaBars, FaWindowClose } from "react-icons/fa";
+import { FaBars, FaHome, FaWindowClose } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
+import logo from "../../public/logo.png";
+import Image from "next/image";
 
-const Sidebar = ({ showMobilemenu }) => {
+const Sidebar = () => {
   const [collapseShow, setCollapseShow] = React.useState("hidden");
   const router = useRouter();
+  const routes = [
+    {
+      path: "/dashboard/dashboard",
+      name: "Dashboard",
+      icon: <FaHome />,
+    },
+    {
+      path: "/dashboard/profile",
+      name: "Profile",
+      icon: <ImProfile />,
+    },
+  ];
   return (
     <>
       <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
@@ -23,9 +37,9 @@ const Sidebar = ({ showMobilemenu }) => {
           {/* Brand */}
           <Link
             href="/"
-            className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+            className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold px-0"
           >
-            Minute Medico
+            <Image src={logo} alt="logo" width={180} />
           </Link>
           {/* User */}
           <ul className="md:hidden items-center flex flex-wrap list-none">
@@ -48,7 +62,7 @@ const Sidebar = ({ showMobilemenu }) => {
                     href="/"
                     className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
                   >
-                    aaaaa
+                    <Image src={logo} alt="logo" width={180} />
                   </Link>
                 </div>
                 <div className="w-6/12 flex justify-end">
@@ -73,100 +87,40 @@ const Sidebar = ({ showMobilemenu }) => {
               </div>
             </form>
 
+            {/* Profile */}
+            <div className=" hidden md:block">
+              <hr className="mb-4 md:min-w-full" />
+              <div className="user_section text-center border p-2 rounded-md">
+                <img
+                  className={`user_logo block mx-auto rounded-full`}
+                  alt="User photo"
+                  width={100}
+                  height={100}
+                  src="https://i.ibb.co/MgsTCcv/avater.jpg"
+                />
+                <h1 className="user_name text-lg whitespace-nowrap">
+                  Abu Shama
+                </h1>
+                <p className="role text-sm whitespace-nowrap">Super Admin</p>
+              </div>
+            </div>
+
             {/* Divider */}
             <hr className="my-4 md:min-w-full" />
-            {/* Heading */}
-            <h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-              Admin Layout Pages
-            </h6>
             {/* Navigation */}
-
             <ul className="md:flex-col md:min-w-full flex flex-col list-none">
-              <li className="items-center">
-                <Link
-                  href="/admin/dashboard"
-                  className={
-                    "text-xs uppercase py-3 font-bold block " +
-                    (router.pathname.indexOf("/admin/dashboard") !== -1
-                      ? "text-lightBlue-500 hover:text-lightBlue-600"
-                      : "text-blueGray-700 hover:text-blueGray-500")
-                  }
-                >
-                  <i
-                    className={
-                      "fas fa-tv mr-2 text-sm " +
-                      (router.pathname.indexOf("/admin/dashboard") !== -1
-                        ? "opacity-75"
-                        : "text-blueGray-300")
-                    }
-                  ></i>{" "}
-                  Dashboard
-                </Link>
-              </li>
-
-              <li className="items-center">
-                <Link
-                  href="/admin/settings"
-                  className={
-                    "text-xs uppercase py-3 font-bold block " +
-                    (router.pathname.indexOf("/admin/settings") !== -1
-                      ? "text-lightBlue-500 hover:text-lightBlue-600"
-                      : "text-blueGray-700 hover:text-blueGray-500")
-                  }
-                >
-                  <i
-                    className={
-                      "fas fa-tools mr-2 text-sm " +
-                      (router.pathname.indexOf("/admin/settings") !== -1
-                        ? "opacity-75"
-                        : "text-blueGray-300")
-                    }
-                  ></i>{" "}
-                  Settings
-                </Link>
-              </li>
-
-              <li className="items-center">
-                <Link
-                  href="/admin/maps"
-                  className={
-                    "text-xs uppercase py-3 font-bold block " +
-                    (router.pathname.indexOf("/admin/maps") !== -1
-                      ? "text-lightBlue-500 hover:text-lightBlue-600"
-                      : "text-blueGray-700 hover:text-blueGray-500")
-                  }
-                >
-                  <i
-                    className={
-                      "fas fa-map-marked mr-2 text-sm " +
-                      (router.pathname.indexOf("/admin/maps") !== -1
-                        ? "opacity-75"
-                        : "text-blueGray-300")
-                    }
-                  ></i>{" "}
-                  Maps
-                </Link>
-              </li>
-            </ul>
-
-            {/* Divider */}
-            <hr className="my-4 md:min-w-full" />
-            {/* Heading */}
-            <h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-              Auth Layout Pages
-            </h6>
-            {/* Navigation */}
-
-            <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-              <li className="items-center">
-                <Link
-                  href="/auth/login"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-                >
-                  <i className="fas fa-fingerprint text-blueGray-400 mr-2 text-sm"></i>{" "}
-                  Login
-                </Link>
-              </li>
+              {routes.map((route, index) => {
+                return (
+                  <li key={index} className="items-center">
+                    <Link
+                      href={route.path}
+                      className="text-sm uppercase py-2 font-semibold flex items-center hover:text-accent hover:bg-slate-50"
+                    >
+                      <span className="mr-2">{route.icon}</span> {route.name}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
